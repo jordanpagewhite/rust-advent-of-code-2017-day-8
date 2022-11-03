e0043b485ba28bfaf6877dc9ac0b4adc7dd61044
Jordan White <jowhite@redhat.com>
2022-11-03 15:22:07 -0700

    Eliminated unnecessary variables
    This is perhaps over-refactoring, but I decided to remove the variables
    that hold each element of the 'parts' we need from each line.

--- src/main.rs
+++ src/main.rs
@@ -8,34 +8,27 @@ fn main() {
 
     for line in input.lines() {
         let parts: Vec<&str> = line.split_whitespace().collect();
-        let operation_var = parts[0];
-        let operation_type = parts[1].to_string();
-        let operation_value = parts[2].to_string().parse::<isize>().unwrap();
-        // nobody cares about the "if" in parts[3].
-        let conditional_var = parts[4];
-        let conditional_type = parts[5].to_string();
-        let conditional_value = parts[6].to_string().parse::<isize>().unwrap();
         // If we do not already have this key/val in our HashMap, create it.
-        vars.entry(operation_var.to_string()).or_default();
-        vars.entry(conditional_var.to_string()).or_default();
+        vars.entry(parts[0].to_string()).or_default();
+        vars.entry(parts[4].to_string()).or_default();
         // Evaluate the conditional expression provided in the instruction.
-        if eval(&format!("{} {} {}", vars[conditional_var], conditional_type.as_str(), conditional_value)).unwrap().as_boolean().unwrap() {
-            match operation_type.as_str() {
-                "inc" => *vars.get_mut(operation_var).unwrap() += operation_value,
-                "dec" => *vars.get_mut(operation_var).unwrap() -= operation_value,
+        if eval(&format!("{} {} {}", vars[parts[4]], parts[5], parts[6].to_string().parse::<isize>().unwrap())).unwrap().as_boolean().unwrap() {
+            match parts[1] {
+                "inc" => *vars.get_mut(parts[0]).unwrap() += parts[2].to_string().parse::<isize>().unwrap(),
+                "dec" => *vars.get_mut(parts[0]).unwrap() -= parts[2].to_string().parse::<isize>().unwrap(),
                 _ => unreachable!()
             }
         }
         // Part 2: Keep track of the greatest value while processing the instructions.
-        let greatest_during_process = vars.iter().max_by(|a, b| a.1.cmp(&b.1));
+        let greatest_during_process = vars.iter().max_by(|a, b| a.1.cmp(b.1));
 
         if *greatest_during_process.unwrap().1 > vars["greatest_during_process"] {
             *vars.get_mut("greatest_during_process").unwrap() = *greatest_during_process.unwrap().1;
         }
     }
 
-    println!("greatest during process: {}", vars["greatest_during_process"]);
+    println!("Part 1 (greatest during process): {}", vars["greatest_during_process"]);
     vars.remove("greatest_during_process");
-    let greatest_el = vars.iter().max_by(|a, b| a.1.cmp(&b.1));
-    println!("greatest element at end: {}", greatest_el.unwrap().1);
+    let greatest_el = vars.iter().max_by(|a, b| a.1.cmp(b.1));
+    println!("Part 2 (greatest element at end): {}", greatest_el.unwrap().1);
 }
