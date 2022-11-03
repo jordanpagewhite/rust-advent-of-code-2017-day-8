8686810922ed5d70b334edd9fc0a1bdf48058f3d
Jordan White <jowhite@redhat.com>
2022-11-03 14:55:46 -0700

    Refactored huge conditional
    I refactored the huge conditional with an eval(), made a few other
    changes and removed some unnecessary comments.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 use std::collections::HashMap;
+use evalexpr::{eval};
 
 fn main() {
-    // Part 1: 1
-    // Part 2: 10
-    // let input = include_str!("../data/example.txt");
-    // Part 1: 6343
-    // Part 2: 7184
     let input = include_str!("../data/input.txt");
     let mut vars: HashMap<String, isize> = HashMap::new();
     vars.insert(String::from("greatest_during_process"), 0);
@@ -19,87 +15,27 @@ fn main() {
         let conditional_var = parts[4];
         let conditional_type = parts[5].to_string();
         let conditional_value = parts[6].to_string().parse::<isize>().unwrap();
-
-        // If we do not already have this "variable" in our HashMap, create it.
-        if !vars.contains_key(operation_var) {
-            vars.insert(operation_var.to_string(), 0);
-        }
-        // If we do not already have this "variable" in our HashMap, create it.
-        if !vars.contains_key(conditional_var) {
-            vars.insert(conditional_var.to_string(), 0);
-        }
-        // We now can be certain that the "variables" include in the operation and conditional
-        // portion of this instruction exist in our `vars` HashMap.
-        match conditional_type.as_str() {
-            ">" => {
-                if vars[conditional_var] > conditional_value {
-                    match operation_type.as_str() {
-                        "inc" => *vars.get_mut(operation_var).unwrap() += operation_value,
-                        "dec" => *vars.get_mut(operation_var).unwrap() -= operation_value,
-                        _ => {}
-                    }
-                }
-            },
-            "<" => {
-                if vars[conditional_var] < conditional_value {
-                    match operation_type.as_str() {
-                        "inc" => *vars.get_mut(operation_var).unwrap() += operation_value,
-                        "dec" => *vars.get_mut(operation_var).unwrap() -= operation_value,
-                        _ => {}
-                    }
-                }
-            },
-            ">=" => {
-                if vars[conditional_var] >= conditional_value {
-                    match operation_type.as_str() {
-                        "inc" => *vars.get_mut(operation_var).unwrap() += operation_value,
-                        "dec" => *vars.get_mut(operation_var).unwrap() -= operation_value,
-                        _ => {}
-                    }
-                }
-            },
-            "<=" => {
-                if vars[conditional_var] <= conditional_value {
-                    match operation_type.as_str() {
-                        "inc" => *vars.get_mut(operation_var).unwrap() += operation_value,
-                        "dec" => *vars.get_mut(operation_var).unwrap() -= operation_value,
-                        _ => {}
-                    }
-                }
-            },
-            "==" => {
-                if vars[conditional_var] == conditional_value {
-                    match operation_type.as_str() {
-                        "inc" => *vars.get_mut(operation_var).unwrap() += operation_value,
-                        "dec" => *vars.get_mut(operation_var).unwrap() -= operation_value,
-                        _ => {}
-                    }
-                }
-            },
-            "!=" => {
-                if vars[conditional_var] != conditional_value {
-                    match operation_type.as_str() {
-                        "inc" => *vars.get_mut(operation_var).unwrap() += operation_value,
-                        "dec" => *vars.get_mut(operation_var).unwrap() -= operation_value,
-                        _ => {}
-                    }
-                }
+        // If we do not already have this key/val in our HashMap, create it.
+        vars.entry(operation_var.to_string()).or_default();
+        vars.entry(conditional_var.to_string()).or_default();
+        // Evaluate the conditional expression provided in the instruction.
+        if eval(&format!("{} {} {}", vars[conditional_var], conditional_type.as_str(), conditional_value)).unwrap().as_boolean().unwrap() {
+            match operation_type.as_str() {
+                "inc" => *vars.get_mut(operation_var).unwrap() += operation_value,
+                "dec" => *vars.get_mut(operation_var).unwrap() -= operation_value,
+                _ => unreachable!()
             }
-            _ => {}
         }
-        // Part 2
-        let greatest_during_process = vars.iter()
-            .max_by(|a, b| a.1.cmp(&b.1));
+        // Part 2: Keep track of the greatest value while processing the instructions.
+        let greatest_during_process = vars.iter().max_by(|a, b| a.1.cmp(&b.1));
 
         if *greatest_during_process.unwrap().1 > vars["greatest_during_process"] {
             *vars.get_mut("greatest_during_process").unwrap() = *greatest_during_process.unwrap().1;
         }
     }
 
-    let greatest_el = vars.iter()
-        .max_by(|a, b| a.1.cmp(&b.1));
-        //.map(|(k, _v)| k);
-
-    println!("greatest during process: {:?}", vars["greatest_during_process"]);
-    println!("{:?}", greatest_el.unwrap());
+    println!("greatest during process: {}", vars["greatest_during_process"]);
+    vars.remove("greatest_during_process");
+    let greatest_el = vars.iter().max_by(|a, b| a.1.cmp(&b.1));
+    println!("greatest element at end: {}", greatest_el.unwrap().1);
 }
